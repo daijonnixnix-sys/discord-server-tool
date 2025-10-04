f40879a860010fb16f7d6fa6ee2e764e80e8b08a
daijonnixnix-sys <daijonnixnix@gmail.com>
2025-10-03 22:59:05 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    // Placeholder for cloning functionalities
+});
+
+client.login(process.env.BOT_TOKEN);
